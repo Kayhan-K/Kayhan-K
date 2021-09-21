45ab8a31b526a7592d306117ab5023d6f5f87fc9
Kajumi <M2100454@middlesbro.ac.uk>
2021-09-21 12:50:13 +0100

    first commit

---
+++ CHATBOT.py
@@ -0,0 +1,23 @@
+print("hello there!")
+
+a = input("whats your name?")
+print("Nice to meet you " +a)
+
+b = input("How are you doing?")
+print("Thats Great!")
+
+Q = input("Since we are here , Do you want to hear a joke.Yes or No will do?.")
+if Q == "Yes":
+    print("Why are humans known to be extremely afraid of computers? Probably, because they byte!")
+if Q == "No":
+    print("Agreed")
+
+          
+
+
+
+
+
+
+
+
---
+++ test1.py
@@ -0,0 +1,5 @@
+name = input("What is your name?")
+print ("my name is " + name)
+
+a = input("goodbye")
+
