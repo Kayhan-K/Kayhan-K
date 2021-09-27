6cfe3c15ce4bcb49dac0c907188d9d2a6b59d454
Kayhan-K <90604678+Kayhan-K@users.noreply.github.com>
2021-09-27 11:12:18 +0100

    Add files via upload

---
+++ Numbers.py
@@ -0,0 +1,16 @@
+def addition(Num1,Num2):
+    print(Num1+Num2)
+
+addition(600,50)
+
+def divide(Num3,Num4):
+    print(Num3/Num4)
+divide(500,10)
+
+def times(Num5,Num6):
+    print(Num5*Num6)
+times(500,2)
+
+def subtract(Num7,Num8):
+    print(Num7-Num8)
+subtract(500,200)
