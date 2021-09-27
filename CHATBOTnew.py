37e39f98324a11c2c261979a43fedcaabd4ef806
Kayhan-K <90604678+Kayhan-K@users.noreply.github.com>
2021-09-27 11:12:41 +0100

    Add files via upload

---
+++ CHATBOTnew.py
@@ -0,0 +1,34 @@
+print("hello there!")
+print("My name is Titamium")
+
+a = input("whats your name?")
+print("Nice to meet you " +a)
+
+b = input("How are you doing?")
+print("That's Great!")
+
+Q = input("Since we are here , Do you want to hear a joke.Yes or No will do?.")
+if Q == "Yes":
+    print("Why are humans known to be extremely afraid of computers?")
+    print("Probably, because they byte!")
+if Q == "No":
+    print("Okay Agreed :) ")
+
+W = input("Do you still want to proceed with Titanium,'Yes' or 'No' will do ?")
+if W == "Yes":
+    print("Affirmative!")
+    print("Here is another joke :)")
+    print("")
+if W == "No":
+    print("Okay , Titanium is shutting down!")
+
+
+          
+
+
+
+
+
+
+
+
