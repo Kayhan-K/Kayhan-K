a847d64344d725dd2fa6635e73bc3103aaf882e9
Kayhan-K <90604678+Kayhan-K@users.noreply.github.com>
2021-10-04 10:04:55 +0100

    Add files via upload

---
+++ 3 inputs high numer print out.py
@@ -0,0 +1,15 @@
+num1=(40)
+num2=(7)
+num3=(58)
+
+if (num1 > num2) and (num2 < num3):
+    print(num1)
+
+elif(num1 < num2) and (num2 > num3):
+    print(num2)
+
+elif(num1 < num2) and (num2 < num3):
+    print(num3)
+
+
+
