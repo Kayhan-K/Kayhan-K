a67bba8fa1c1e86d0707e7d3418783a4c0db8d61
Kayhan-K <90604678+Kayhan-K@users.noreply.github.com>
2021-11-22 10:23:42 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+#Write a for loop which appends the first 1000 numbers to a list 
+
+#Numbers = []
+#for i in range (1000):
+      #Numbers.append(i)
+#print(Numbers)
+
+#Write the same code again, but using list comprehension 
+
+# Numbers = [x for x in range (1000) if x % 1 == 0 ]
+#print(Numbers)
+
