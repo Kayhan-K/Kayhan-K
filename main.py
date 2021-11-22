15d91710f8e087423a575d85e2c3d9ce8d147689
Kayhan-K <90604678+Kayhan-K@users.noreply.github.com>
2021-11-22 09:28:10 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+def check(red, dead):  #This is the inital function that checks if the two words are anagrams or not.
+ if(sorted(red)==sorted(dead)):
+   print ("The strings are Anagrams.")
+
+ else:
+   print("The strings aren't Anagram.")
+#If the strings are not anagrams then the else statment will print that they are not anagrams.
+
+check("red", "dead")
+
+# Function that will take two strings and return whether they are anagrams - not boolean 
