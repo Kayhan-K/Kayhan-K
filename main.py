b1b36b80eebf30130883c0a8b898db14794f931e
Kayhan-K <90604678+Kayhan-K@users.noreply.github.com>
2021-11-22 11:53:54 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+#1.Write a for loop which appends the first 1000 numbers to a list 
+
+#Numbers = []
+#for i in range (1000):
+      #Numbers.append(i)
+#print(Numbers)
+
+#2 .Write the same code again, but using list comprehension 
+
+# Numbers = [x for x in range (1000) if x % 1 == 0 ]
+#print(Numbers)
+
+#3.Find all of the numbers from 1–1000 that are divisible by 8
+
+#Divison8 = [x for x in range (1000) if x % 8 == 0 ] 
+#print(Divison8)
+
+#4.Find all of the numbers from 1–1000 that have a 6 in them
+
+#Num6 = [x for x in range(1,1000) if '6'  in str(x)]
+#print(Num6)
+
